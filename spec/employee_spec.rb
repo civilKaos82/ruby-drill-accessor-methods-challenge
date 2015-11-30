0929ce8381cad09283d52b4e7cca0df0ac9acb72
Torey Hickman <torey@toreyhickman.com>
2015-11-30 07:41:05 -0600

    Provide test for writable name

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -7,5 +7,9 @@ describe Employee do
     it 'has a readable name' do
       expect(employee.name).to eq('Elle Gilbert')
     end
+
+    it 'has a writable name' do
+      expect { employee.name = 'Brittny Gilbert' }.to change { employee.name }.to 'Brittny Gilbert'
+    end
   end
 end
